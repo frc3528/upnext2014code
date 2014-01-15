540404d343cb54cae47a90d5bc1c55f58b682cfe
Kooper Holbrook <kwh29@hotmail.com>
2014-01-14 19:54:02 -0600

    DriveTrain

--- src/org/usfirst/frc3528/UpNext2014Robot/commands/DriveWithJoystick.java
+++ src/org/usfirst/frc3528/UpNext2014Robot/commands/DriveWithJoystick.java
@@ -10,6 +10,8 @@
 package org.usfirst.frc3528.UpNext2014Robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc3528.UpNext2014Robot.Robot;
+import org.usfirst.frc3528.UpNext2014Robot.OI;
+import org.usfirst.frc3528.UpNext2014Robot.subsystems.MecanumDrive;
 /**
  *
  */
@@ -27,6 +29,7 @@ public class  DriveWithJoystick extends Command {
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        MecanumDrive.driveWithJoystick(OI.getJoyStick(1));
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
