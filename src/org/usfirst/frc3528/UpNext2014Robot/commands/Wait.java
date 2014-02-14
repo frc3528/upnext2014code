186c0d1362c24d23a1384f75817dd9c5c70f9c47
Nathanael Holden <njlh98@icloud.com>
2014-02-13 20:28:53 -0600

    Added Command group for Autonomous command. Added Wait command.

--- src/org/usfirst/frc3528/UpNext2014Robot/commands/Wait.java
+++ src/org/usfirst/frc3528/UpNext2014Robot/commands/Wait.java
@@ -1,32 +1,43 @@
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package org.usfirst.frc3528.UpNext2014Robot.commands;
+
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.CommandGroup;
+
 /**
  *
+ * @author Team Up Next
  */
-public class  Autonomous extends CommandGroup {
-    public Autonomous() {
+public class Wait extends Command {
+    
+    private double time;
+    
+    public Wait(double time) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-	addSequential(new CameraLightOn());
-        addSequential(new Wait(5));
-        addSequential(new CameraLightOff());
+        this.time = time;
     }
+
     // Called just before this Command runs the first time
     protected void initialize() {
-        
-       }
+        setTimeout(time);
+    }
+
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-         }
+    }
+
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
+
     // Called once after isFinished returns true
     protected void end() {
     }
+
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
