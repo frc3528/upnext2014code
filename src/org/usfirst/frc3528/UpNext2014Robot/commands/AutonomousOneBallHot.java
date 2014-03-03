512b08e24b9be0490f83a69e79337015d29c2099
Kooper Holbrook <kwh29@hotmail.com>
2014-03-03 17:17:49 -0600

    added one ball auto

--- src/org/usfirst/frc3528/UpNext2014Robot/commands/AutonomousOneBallHot.java
+++ src/org/usfirst/frc3528/UpNext2014Robot/commands/AutonomousOneBallHot.java
@@ -1,22 +1,19 @@
 
 package org.usfirst.frc3528.UpNext2014Robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc3528.UpNext2014Robot.RobotMap;
 /**
  *
  */
 public class  AutonomousOneBallHot extends CommandGroup {
     public AutonomousOneBallHot() {
         
-        addSequential(new CameraLightOn());
+        //addSequential(new CameraLightOn());
+        //addSequential (new LowerPickerUpper());
+        addSequential(new DriveByFeet(11, 0.90));
         addSequential (new LowerPickerUpper());
-        
-        //if () {
-            
-        //} else {
-            //addSequential(new DriveByFeet(12 , 0.75));
-
-        //}
-
+        addSequential(new Wait(RobotMap.WAIT_BETWEEN_FIRE));
+        addSequential(new Fire());
 
     }
 }
