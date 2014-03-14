4d1515167a525684d9d841814c8ef60e2ad35dd6
Kooper Holbrook <kwh29@hotmail.com>
2014-03-14 01:32:56 -0500

    experminenting at kc regional

---
+++ src/org/usfirst/frc3528/UpNext2014Robot/commands/AutonomousTesting.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc3528.UpNext2014Robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ * @author developer
+ */
+public class AutonomousTesting extends CommandGroup {
+    
+    public AutonomousTesting() {
+        addParallel(new LowerPickerUpper());
+        addSequential(new ReversePickerUpper(0.1));
+        
+    }
+}
