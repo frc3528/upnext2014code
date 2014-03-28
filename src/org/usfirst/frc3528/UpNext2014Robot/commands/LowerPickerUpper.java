28c8e4f901a6f291314c8bbe716324169824bd45
Kooper Holbrook <kwh29@hotmail.com>
2014-03-27 23:28:08 -0500

    Changed pick up limit switch to new idea

--- src/org/usfirst/frc3528/UpNext2014Robot/commands/LowerPickerUpper.java
+++ src/org/usfirst/frc3528/UpNext2014Robot/commands/LowerPickerUpper.java
@@ -42,7 +42,7 @@ public class LowerPickerUpper extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-        //Robot.pickerUpper.disableArm();
+        Robot.pickerUpper.disableArm();
     }
 
     // Called when another command which requires one or more of the same
